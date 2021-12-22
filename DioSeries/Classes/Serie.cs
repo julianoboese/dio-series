f1e5f6914a17d40babc39ca68975f96387588895
Juliano Boese <juliano.boese@gmail.com>
2021-12-22 19:22:09 -0300

    Cria método ToString (override) na classe Serie

--- DioSeries/Classes/Serie.cs
+++ DioSeries/Classes/Serie.cs
@@ -1,4 +1,6 @@
-﻿namespace DioSeries
+﻿using System;
+
+namespace DioSeries
 {
     public class Serie : EntidadeBase
     {
@@ -16,6 +18,16 @@
             this.Titulo = titulo;
             this.Descricao = descricao;
             this.Ano = ano;
+        }
+
+        public override string ToString()
+        {
+            string retorno = "";
+            retorno += "Gênero: " + this.Genero + Environment.NewLine;
+            retorno += "Título: " + this.Titulo + Environment.NewLine;
+            retorno += "Descrição: " + this.Descricao + Environment.NewLine;
+            retorno += "Ano de início: " + this.Ano + Environment.NewLine;
+            return retorno;
         }
     }
 }
