abc8aca3f91fe0d320d290ed9683c52fe71f3b36
Juliano Boese <juliano.boese@gmail.com>
2021-12-23 14:45:25 -0300

    Cria função para obter a opção do usuário no Program

--- DioSeries/Program.cs
+++ DioSeries/Program.cs
@@ -6,7 +6,36 @@ namespace DioSeries
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string opcaoUsuario = ObterOpcaoUsuario();
+
+            while (opcaoUsuario.ToUpper() != "X")
+            {
+                switch (opcaoUsuario)
+                {
+                    default:
+                        break;
+                }
+            }
+        }
+
+        private static string ObterOpcaoUsuario()
+        {
+            Console.WriteLine();
+            Console.WriteLine("DIO Séries");
+            Console.WriteLine("Informe a opção desejada:");
+
+            Console.WriteLine("1 - Listar séries");
+            Console.WriteLine("2 - Inserir nova série");
+            Console.WriteLine("3 - Atualizar série");
+            Console.WriteLine("4 - Excluir série");
+            Console.WriteLine("5 - Visualizar série");
+            Console.WriteLine("C - Limpar tela");
+            Console.WriteLine("X - Sair");
+            Console.WriteLine();
+
+            string opcaoUsuario = Console.ReadLine().ToUpper();
+            Console.WriteLine();
+            return opcaoUsuario;
         }
     }
 }
