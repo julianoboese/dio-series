2defb6d5e18ac7927fececc3a6bae488269f7aa2
Juliano Boese <juliano.boese@gmail.com>
2021-12-23 10:31:46 -0300

    Cria interface repositório e repositório de séries

---
+++ DioSeries/Classes/SerieRepositorio.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using DioSeries.Interfaces;
+
+namespace DioSeries
+{
+    internal class SerieRepositorio : IRepositorio<Serie>
+    {
+        private List<Serie> listaserie = new List<Serie>();
+        public void Atualiza(int id, Serie entidade)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Exclui(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insere(Serie entidade)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Serie> Lista()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int ProximoId()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Serie RetornaPorId(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
